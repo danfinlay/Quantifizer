14e793e178012ed005d426128c2ec519b4469bf0
Dan Finlay <dan@danfinlay.com>
2015-12-06 21:44:18 -0800

    Add easy user impersonation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::API
 
   before_filter :authenticate_user_from_token, except: [:token]
 
-
   def token
     authenticate_with_http_basic do |email, password|
       user = User.find_by(email: email)
@@ -21,6 +20,13 @@ end
 private
    
 def authenticate_user_from_token
+
+  # Allow easy impersonation in development
+  if Rails.env.development? and
+     params[:mode] == 'test'
+    return User.first
+  end
+
   unless authenticate_with_http_token { |token, options| User.find_by(auth_token: token) }
     render json: { error: 'Bad Token'}, status: 401
   end 
